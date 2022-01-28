b08a75e995477199e980eb049faeb0a4e1a56aeb
unknown <Gabriel Martin Philot>
2022-01-28 18:01:10 -0300

    nome da versão

---
+++ aula_github.py
@@ -0,0 +1,9 @@
+# Lista de comandos que vamos utilizar
+
+# primeiro comando diferente : git init
+
+# segundo comando difente: git remote add origin linkdogithub
+
+# terceiro comando diferente: git pull origin main --allow-unrelated-histories
+
+# quarto comando diferente: 
